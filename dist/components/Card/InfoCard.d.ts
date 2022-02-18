891a8a6186618c9f16db6ab453f559af94962fb2
Bruno Almeida Kotesky <blasterplayersbr@gmail.com>
2022-02-18 10:08:21 -0300

    Build changes

--- dist/components/Card/InfoCard.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-import type { IInfoCardProps } from '../../models/interfaces/IInfoCardProps';
-export declare const InfoCard: React.MemoExoticComponent<(props: IInfoCardProps) => JSX.Element>;
