f3380618d83bad4f39386823b78e1143b3be6f55
Bruno Almeida Kotesky <blasterplayersbr@gmail.com>
2022-02-23 17:25:45 -0300

    Build changes

--- dist/components/index.d.ts
+++ dist/components/index.d.ts
@@ -1,3 +1,4 @@
 export { UploadButton } from './UploadButton/UploadButton';
 export { GridView } from './GridView/GridView';
 export { DataTable } from './DataTable/DataTable';
+export { IFrame } from './IFrame/IFrame';
