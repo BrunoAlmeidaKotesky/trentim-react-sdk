27239d4094e60a768e40eeca7a719c9f55988393
Bruno Almeida Kotesky <blasterplayersbr@gmail.com>
2022-03-17 15:35:04 -0300

    Build changes

--- dist/models/interfaces/ISPUser.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ISiteUserInfo } from "@pnp/sp/site-users/types";
-export interface ISPUser extends ISiteUserInfo {
-    ProfilePic?: string;
-    IsExternalUser: boolean;
-}
--- src/services/index.ts
+++
@@ -1,1 +0,0 @@
-export {BaseService} from './BaseService';
