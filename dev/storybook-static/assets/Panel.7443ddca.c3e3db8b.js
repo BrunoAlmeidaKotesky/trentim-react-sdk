e50c5a5a9ecfd74a35f77a024932d80627dae153
Bruno Kotesky <bruno.kotesky@trentim.com>
2022-08-19 11:37:58 -0300

    Storybook static

---
+++ dev/storybook-static/assets/Panel.7443ddca.c3e3db8b.js
@@ -0,0 +1,2 @@
+import{o as s,q as l,m}from"./Panel.98c00fb5.388b69ab.js";import"./iframe.e24182c1.js";export{s as Panel,l as PanelBase,m as PanelType};
+//# sourceMappingURL=Panel.7443ddca.c3e3db8b.js.map
